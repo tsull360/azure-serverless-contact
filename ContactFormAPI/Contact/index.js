11ca3b3dd3ea1e985236b72f4ea5682adbc3e0a5
Tim S <tsull360@outlook.com>
2023-07-09 15:31:04 -0700

    Initial Updates

--- ContactFormAPI/Contact/index.js
+++ ContactFormAPI/Contact/index.js
@@ -1,13 +1,34 @@
 module.exports = async function (context, req) {
     if (req.body.email) {
+
+        let fromAddress = process.env.mailfrom;
+        let toAddress = process.env.mailto;
+        context.log("Have send address, will proceed.")
+        context.log("Mail from value pulled from settings: ", fromAddress)
+        context.log("Mail to value pulled from settings: ", toAddress)
+
+        var mailbody = `Hi,
+        You have a new webform submission from your website.
+        
+        Sender: ${req.body.name}
+        Email Address: ${req.body.email}
+        Event Date: ${req.body.edate}
+        Message:
+        ${req.body.message}
+        
+        Thanks!
+        `
         var email = {
             from: {
-                email: req.body.email
+                email: fromAddress
+            },
+            to: {
+                email: toAddress
             },
             subject: "Contact form submission from: " + req.body.name,
             content: [{
                 type: 'text/plain',
-                value: req.body.message
+                value: mailbody
             }]
         };
 
